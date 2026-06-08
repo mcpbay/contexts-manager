78088404c7da57e03a57fb2f3f675f55ac95f403
mcpbay <mcpbayio@gmail.com>
2026-06-07 23:41:34 -0300

    fix: little issue with deno reference

--- src/utils/fs.util.ts
+++ src/utils/fs.util.ts
@@ -1,3 +1,4 @@
+/// <reference lib="deno.ns" />
 import * as fs from "node:fs";
 import * as os from "node:os";
 import * as path from "node:path";
@@ -39,9 +40,8 @@ export function readDirSync(resolvedPath: string) {
 }
 
 export function makeTempFileSync(suffix: string) {
-  const uniqueId = `${Date.now()}-${++tempCounter}-${
-    Math.random().toString(36).slice(2, 8)
-  }`;
+  const uniqueId = `${Date.now()}-${++tempCounter}-${Math.random().toString(36).slice(2, 8)
+    }`;
   const filePath = path.join(os.tmpdir(), `mcpb-${uniqueId}.${suffix}`);
 
   fs.writeFileSync(filePath, "", "utf-8");
@@ -64,9 +64,8 @@ export function readTextFileSync(filePath: string) {
 }
 
 export function makeTempDirSync() {
-  const uniqueId = `${Date.now()}-${++tempCounter}-${
-    Math.random().toString(36).slice(2, 8)
-  }`;
+  const uniqueId = `${Date.now()}-${++tempCounter}-${Math.random().toString(36).slice(2, 8)
+    }`;
   const dirPath = path.join(os.tmpdir(), `mcpb-${uniqueId}`);
 
   fs.mkdirSync(dirPath);
@@ -102,7 +101,7 @@ const __isDeno = getRuntime() === Runtime.Deno;
 
 export function createDenoCommand(
   args: string[],
-  options: { cwd?: string | URL },
+  options: { cwd?: string | URL; },
 ) {
   if (__isDeno) {
     return createDenoCommandOnDeno(args, options);
@@ -113,7 +112,7 @@ export function createDenoCommand(
 
 function createDenoCommandOnDeno(
   args: string[],
-  options: { cwd?: string | URL },
+  options: { cwd?: string | URL; },
 ) {
   const command = new Deno.Command("deno", {
     args,
@@ -145,7 +144,7 @@ function createDenoCommandOnDeno(
 
 function createDenoCommandOnNode(
   args: string[],
-  options: { cwd?: string | URL },
+  options: { cwd?: string | URL; },
 ) {
   return {
     spawn() {
